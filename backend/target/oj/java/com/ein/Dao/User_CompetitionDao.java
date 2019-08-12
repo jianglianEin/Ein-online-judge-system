1e9cf639fd8bd2dcbe95a84a029594283d9f0e6f
jianglianEin <913057041@qq.com>
2019-08-12 21:26:56 +0800

    [JiShi] fixed mvn data

--- backend/target/oj/java/com/ein/Dao/User_CompetitionDao.java
+++
@@ -1,7 +0,0 @@
-package com.ein.Dao;
-
-import com.ein.Model.User_Competition;
-
-public interface User_CompetitionDao extends BaseDao<User_Competition>{
-
-}
--- backend/target/oj/java/com/ein/Service/SolutionOfCompetitionService.java
+++
@@ -1,8 +0,0 @@
-package com.ein.Service;
-
-import com.ein.Model.Problem;
-import com.ein.Model.SolutionOfCompetition;
-
-public interface SolutionOfCompetitionService extends BaseService<SolutionOfCompetition>{
-
-}
